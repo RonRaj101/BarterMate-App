e9604db0fc26cb4311aa337fdc274971cae36806
Ronit Rai <43289911+RonRaj101@users.noreply.github.com>
2023-04-14 19:43:34 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'barter-mate-draft',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
